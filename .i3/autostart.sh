a147a80f79ded790a5940968360949bb1fcc5bc4
deterenkelt <deterenkelt.github@gmail.com>
2014-03-22 01:22:28 +0400

    Rewrite .i3/autostart.sh; Add global vars WIDTH and HEIGHT
    modified:   .preinit.sh
      Now this file exports two variables containing main screen dimensions.
      This is useful when creating layout for windows on the main workspace as well
        as at building WINEPREFIX, automatically setting dimensions to the virtual
    	workspace. I will probably add ~/bashrc/wine.sh in the future.
    
    modified:   .i3/autostart.sh
      Just slightly changed the order of applications to be run at startup and
        dividing them per hosts and distinguishing common ones.
      Here must be a mention that this script now uses faked binaries from ~/bin,
        which should already be prefixed in PATH in one of the previous commits, it
        will be explained in the next, about cryptography, tmpfs and GPG.
    
    I need to do something. Telling git what a hunk must look like is a pain…

--- .i3/autostart.sh
+++ .i3/autostart.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-## This script is called from ~/.i3/config with
-##     exec ~/.i3/autstart.sh
-~/.i3/set_keyboard.sh
+# autostart.sh
+# This script is called from ~/.i3/config with ‘exec ~/.i3/autstart.sh’
 
-## Load my ssh identities
-[ "$1" = stop_after_main_workspace ] || . ~/scripts/ssh-load.sh
+# set -x
+# exec &>~/i3autostartlog
 
 # Check for two monitors and wacom.
 # xsetwacom --set <ID> MapToOuput WxH+0+0
 # W = 1st_screen_width/(2nd_screen_width/1st_screen_width)
 # H = ———"———
 
+# Temporarily disable pointer while setting layout
 pointer_control() {
 	[ -v pointer_devices ] || pointer_devices=$(xinput --list |\
 	sed -nr '/Virtual\score.*pointer/ !s/.*id=([0-9]+)\s+\[slave\s+pointer.*/\1/p')
 	for dev in $pointer_devices; do xinput --$1 $dev; done
 }
 
+# Wait for the last command sent to background to create a window
 wait_for_program () {
 	n=0
 	while true; do
-		# Waiting for the last sent to background command create a window
 		if xdotool search --onlyvisible --pid $!; then
 			break
 		else
@@ -36,7 +36,13 @@ wait_for_program () {
     done
 }
 
+# Cleaning before new session.
+# That’s strange, Emacs runned from autostart script hangs after session 
+#   is closed, but is okay if it was started via /etc/init.d/emacs.user
+killall emacs emacsclient
 sudo /usr/bin/killall iftop
+
+# Applications that need to be started before layout setting: urxvtd and tmux
 pgrep urxvtd || urxvtd -q -o -f
 tmux="tmux -u -f $HOME/.tmux/config -S $HOME/.tmux/socket"
 pgrep -u $UID -f '^tmux.*$' &>/dev/null || $tmux \
@@ -49,21 +55,15 @@ pgrep -u $UID -f '^tmux.*$' &>/dev/null || $tmux \
 	select-window -t root
 pointer_control disable
 
-# Getting screen dimensions.
-# This assumes the first screen listed is the main in use.
-read width height  <<< `xrandr | \
-	sed -nr 's/^\s+([0-9]+)x([0-9]+).*\*.*/\1\n\2/p;T;Q0'`
-
-common_startup_apps=("thunar" "mpd")
-
+# WIDTH and HEIGHT were set in the ~/.preinit.sh
 case $HOSTNAME in
 	fanetbook)
 		hsetroot -fill ~/.env/wallpaper.png
 		urxvtc -hold -name 'htop' -title "htop" -e htop
-		xte "mousemove $(( width/2 ))  $(( height/2 ))" 
+		xte "mousemove $(( WIDTH/2 ))  $(( HEIGHT/2 ))"
 		xte 'mouseclick 1'
 		i3-msg layout tabbed
-		
+
 		for iface_config in `ls ~/.iftop/$HOSTNAME.*`; do
 			urxvtc -hold -title ${iface_config##*.} \
 				-e sudo /usr/sbin/iftop -c "$iface_config"
@@ -71,22 +71,22 @@ case $HOSTNAME in
 		urxvtc
 		urxvtc
 		urxvtc -hold -title tmux -e $tmux attach \; find-window -N root
-		
+
 		startup_apps=()
 		;;
 	*)
-		pgrep -u $UID -f 'wallpaper_setter.sh' \
+		pgrep -u $UID -f "^bash $HOME/scripts/wallpaper_setter.sh -B" \
 			&& ~/scripts/wallpaper_setter.sh -w \
 			|| { ~/scripts/wallpaper_setter.sh -B -0.3 \
 			   -e "i3-nagbar -m \"%m\" -b Restart \"%a\"" \
 			   -d /home/picts/watched & }
-		urxvtc 
+		urxvtc
 		i3-msg split v
 		urxvtc -hold -title 'htop' -e htop
  		# Now we have two terminals   ÷
 
 		# Move cursor near to the center of the lower urxvtc with htop
- 		xte "mousemove $(( width/2 ))  $(( 3*height/4 ))"
+ 		xte "mousemove $(( WIDTH/2 ))  $(( 3*HEIGHT/4 ))"
 		# Focus it
  		xte 'mouseclick 1'
  		i3-msg split h
@@ -99,33 +99,33 @@ case $HOSTNAME in
 				i3-msg split h
 		done
 		[ -v iftops_need_their_own_container ] && {
-			xte "mousemove $(( 9*width/16 )) $(( 3*height/4 ))"
+			xte "mousemove $(( 9*WIDTH/16 )) $(( 3*HEIGHT/4 ))"
 			xte 'mouseclick 1'
-			i3-msg layout tabbed			
+			i3-msg layout tabbed
 		}
 
-		# Moving cursor to the upper empty urxvtc and raise it to ≈5/6 
+		# Moving cursor to the upper empty urxvtc and raise it to ≈5/6
 		#   of the height
-		xte "mousemove $(( width/2 ))  $(( height/4 ))" 
+		xte "mousemove $(( WIDTH/2 ))  $(( HEIGHT/4 ))" 
 		xte 'mouseclick 1'
 		i3-msg resize grow height 30 px or 30 ppt
 		i3-msg split h
 		urxvtc
 		# Now in upper hald we have two terminals, too ⋅|⋅
 		# Splitting left upper urxvtc
-		xte "mousemove $(( width/4 )) $(( height/2 ))"
+		xte "mousemove $(( WIDTH/4 )) $(( HEIGHT/2 ))"
 		xte 'mouseclick 1'
 		i3-msg split h
 		i3-msg layout tabbed
 		urxvtc
 
-		xte "mousemove $(( 3*width/4 )) $(( height/2 ))"
+		xte "mousemove $(( 3*WIDTH/4 )) $(( HEIGHT/2 ))"
 		xte 'mouseclick 1'
 		i3-msg split h
 		i3-msg layout tabbed
 		urxvtc
 		urxvtc -hold -title tmux -e $tmux attach
- 		xte "mousemove $(( 11*width/12 )) $(( height/2 ))"
+ 		xte "mousemove $(( 11*WIDTH/12 )) $(( HEIGHT/2 ))"
  		xte 'mouseclick 1'
 
 		# This is for calling via hotkey in ~/.i3/config to test without
@@ -135,17 +135,13 @@ case $HOSTNAME in
 			exit 0
 		}
 
-		startup_apps=("emacs --daemon" firefox pidgin "emacsclient -c")
+		startup_apps=("emacs --daemon" firefox pidgin "emacsclient -c -display $DISPLAY")
 		;;
 esac
 pointer_control enable
 
-killall -HUP emacsclient
-for app in "${common_startup_apps[@]}" "${startup_apps[@]}"; do
-	# { $app & } becasue otherwise ‘&’ will fork to background the whole string
-	#   including subshell created by the left part of ‘||’ statement.
-	pgrep -u $UID -f "\<$app\>" >/dev/null || { $app & }
-done
+# Common apps: thunar and mpd
+pgrep -u $UID thunar >/dev/null || { (nohup thunar) & }
 
 until mpc &>/dev/null; do
 	sleep 1
@@ -153,3 +149,10 @@ done
 pgrep -u $UID mpdscribble >/dev/null || \
 	mpdscribble --conf ~/.mpd/mpdscribble.conf
 pgrep -u $UID ncmpcpp >/dev/null || urxvtc -name ncmpcpp -e ncmpcpp
+
+for app in "${startup_apps[@]}"; do
+	# { … & } becasue otherwise ‘&’ will fork to background the whole string
+	#   including subshell created by the left part of ‘||’ statement.
+	# (nohup $app) actuallyy needed only for emacs as daemon
+	pgrep -u $UID -f "^$app\>" >/dev/null || { (nohup $app) & }
+done
--- .preinit.sh
+++ .preinit.sh
@@ -1,3 +1,17 @@
+#should be sourced
+
+# preinit.sh
+# This script is intended to be a common part of ~/.xinitrc and ~/.xsession.
+# It starts deamons needed to be started before the window manager.
+# ~/.bashrc runs this script for ssh clients.
+
+# Getting screen dimensions.
+# This assumes the first screen listed is the main in use.
+read WIDTH HEIGHT  <<< `xrandr | \
+	sed -nr 's/^\s+([0-9]+)x([0-9]+).*\*.*/\1\n\2/p;T;Q1' && echo -e '800\n600'`
+export WIDTH HEIGHT
+
+# Attempt to set multimonitor
 #xrandr --output VGA1 --mode `xrandr | \
 #  sed -nr '/VGA1 c/{N;s/.*\n\s+(\S+)\s+.*/\1/p}'`
 
