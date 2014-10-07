0a5673cf195faecc39e8dd3f840904c116a529e6
deterenkelt <deterenkelt.github@gmail.com>
2014-10-08 00:12:22 +0400

    Changed VM related stuff
    Bind spicec windows  → 5: Misc
    
    Make XP widely accessible in LAN.
    Failed attempt to make virtio driver working
    Y U NO FIX UR DRIVER REDHAT?
    Er… some other stuff, must have written that out.

--- bashrc/home.sh
+++ bashrc/home.sh
@@ -25,6 +25,20 @@ alias wa-a='~/scripts/watch.sh \
             --screenshot-dir-skel="macro,misc"'
 #            -d /old_home/video/anime/ \
 
+# For output on plasma tv, see also ~/.i3/config.template
+function wa-ap() {
+	xrandr --output HDMI-0 --mode 1920x1080 --right-of DVI-I-0
+    ~/scripts/watch.sh \
+        -A -e -m "--x11-name big_screen --profile=hdmi" \
+        --bashrc=$HOME/bashrc/mplayer.sh \
+        --last-ep \
+        -d /home/video/anime/ \
+        -s "season - disk disc cd part pt dvd" \
+        -S /home/picts/watched/ \
+        --screenshot-dir-skel="macro,misc" $@
+    xrandr --output HDMI-0 --off
+}
+
 alias wa-f='~/scripts/watch.sh \
             -A -e -m "--fs" \
             --bashrc=$HOME/bashrc/mplayer.sh \
@@ -33,7 +47,6 @@ alias wa-f='~/scripts/watch.sh \
             -s "season - disk disc cd part pt dvd" \
             -S /home/picts/watched/ \
             --screenshot-dir-skel="macro,misc"'
-
 
 alias sync_fanetbook="sudo -u git /root/scripts/manual_sync.sh fanetbook all"
 alias sync_external_hdd="sudo /root/scripts/manual_sync.sh rescue all"
@@ -49,11 +62,11 @@ alias detach_hdd="sudo /etc/udev/rules.d/rescue_mount.sh sud" # sync-umount-deta
 #	… -netdev user,id=vmnic,hostname=vmdebean -device virtio-net,netdev=vmnic
 # While no qxl driver for Xorg and spice-vdagent installed, -sdl may be used.
 #alias spicec="spicec --hotkeys 'toggle-fullscreen=shift+f11,release-cursor=shift+f12'"
+# ~/bin/qemu-shell/qmp-shell
+# (QEMU) change ide0-cd ~/path/to/iso.iso
 alias qemu-graphic="qemu-kvm -daemonize -enable-kvm \
 	-cpu host \
-	-smp 1,cores=1,threads=1 \
 	-boot order=dc \
-	-m 1024 \
 	-no-frame -no-quit "
 
 alias qemu-nographic="qemu-kvm  -enable-kvm \
@@ -73,25 +86,30 @@ alias vm-i="qemu-nographic -serial stdio \
 #	&& spicec -h 127.0.0.1 -p 5900 -t 'QEMU_initramfs' \
 #	; pkill -9 -f qemu
 
-alias vm-d="qemu-graphic  -vga qxl -spice addr=127.0.0.1,port=5901,disable-ticketing \
+alias vm-d="qemu-graphic	-smp 1,cores=1,threads=1 -m 1024 \
+	-vga qxl -spice addr=127.0.0.1,port=5901,disable-ticketing \
 	-name 'Debean,process=vm-debean' \
 	-drive file=$HOME/vm_debean.img,if=virtio \
 	-netdev vde,id=taputapu,sock=/tmp/vde.ctl \
 		-device virtio-net-pci,netdev=taputapu,mac=DE:BE:AD:EB:EA:DE"
 alias vm-dc='spicec -h 127.0.0.1 -p 5901 -t QEMU_Debean'
 
-alias vm-f="qemu-graphic  -vga qxl -spice addr=127.0.0.1,port=5902,disable-ticketing \
+alias vm-f="qemu-graphic	-smp 1,cores=1,threads=1 -m 1024 \
+	-vga qxl -spice addr=127.0.0.1,port=5902,disable-ticketing \
 	-name 'Feedawra,process=vm-feedawra' \
 	-drive file=$HOME/vm_feedawra.img,if=virtio \
 	-netdev vde,id=taputapu,sock=/tmp/vde.ctl \
 		-device virtio-net-pci,netdev=taputapu,mac=FE:ED:AF:EE:DA:FE"
 alias vm-fc='spicec -h 127.0.0.1 -p 5902 -t QEMU_Feedawra'
 
-#,if=virtio
-alias vm-w="qemu-graphic  -vga qxl -spice addr=127.0.0.1,port=5903,disable-ticketing \
-	-name 'Win_XP,process=vm-winxp' \
-	-drive file=$HOME/vm_winxp.img \
-	-fda viostor.img \
+# ,if=virtio
+alias vm-w="qemu-graphic	-smp 1,cores=2,threads=1 -m 3072 \
+	-vga qxl -spice addr=192.168.0.1,port=5903,disable-ticketing \
+	-qmp unix:./qmp-sock,server,nowait \
+	-name 'Win_XP,process=vm-winxp' -rtc base=localtime -usbdevice tablet \
+	-drive file=$HOME/vm_winxp.img,if=ide,boot=on \
+-drive file=$HOME/fake.qcow2,if=virtio \
+-drive file=/home/soft_win/virtio-win-0.1-81.iso,media=cdrom,index=1 \
 	-netdev vde,id=taputapu,sock=/tmp/vde.ctl \
 		-device virtio-net-pci,netdev=taputapu,mac=11:11:11:11:11:11"
-alias vm-wc='spicec -h 127.0.0.1 -p 5903 -t QEMU_WinXP'
+alias vm-wc='spicec -h 192.168.0.1 -p 5903 -t QEMU_WinXP'
