3d8460f4357cfdd22dd36cd3b7527ffbb2761304
deterenkelt <deterenkelt.github@gmail.com>
2014-10-08 00:29:48 +0400

    ~/bin/run_app now behaves more smarter if files in /tmp/decrypted changed
    If the change of firefox’s files in /tmp/decrypted has changed
      insignificantly (i.e. within ten bytes), then the change is rejected.
      If it’s more than 10 bytes — then if it’s a positive change, it will be
      automatically accepted, and if it’s negative, i.e. decreasing the size
      of files you will be asked whether the script should accept the change.
    
    Truly effective when ~/.i3/on_quit.sh is called before exit
      in ~/.i3/config.template to kill apps alunched from ~/.i3/autostart.sh
      gracefully.
    
    Er… Damn, looks like I need to write every damn commit message while I
      remember which files were in the same bunch.

--- bin/run_app.sh
+++ bin/run_app.sh
@@ -1,28 +1,24 @@
 #!/usr/bin/env bash
 
 # run_app.sh
-# This substitute for the actual binaries allows to decrypt private data 
-#   in configuration files from the repository before the actual application 
+# This substitute for the actual binaries allows to decrypt private data
+#   in configuration files from the repository before the actual application
 #   starts. Decrypted file are to be placed into /tmp/decrypted/ which is
 #   supposed to reside on a temporary filesystem. The data are then encrypted
 #   after application is closed (in case it were modified during the runtime).
 # This script is a single file, its behaviour decided depending on what filename
 #   it was called as.
-# This all works on an assumption that 
-#   a) encrypted files are in the repository and have .gpg extension. Actual 
+# This all works on an assumption that
+#   a) encrypted files are in the repository and have .gpg extension. Actual
 #      file in repository represented by a symlink following to /tmp/decrypted/.
 #   b) ~/.xinitrc has
 #        export PATH="$HOME/bin:$PATH"
 #      in order to make this hack work transparently for any shell or program
 #      invoking the command that is supposed to call the binary.
 
-
 # $1 — absolute path to the actual binary
 # $2..n — absolute paths to files with private information.
 #         These files supposed to reside in repository and have .gpg extension.
-
-# exec &>~/runapp.log
-# set -x 
 run_app() {
 	which "$1" >/dev/null && {
 		local app=$1
@@ -37,30 +33,62 @@ run_app() {
 				lastmods[${#lastmods[@]}]=`stat -c %Y "$tmpfile"`
 			}
 		done
-		while pgrep -f "^bash ${0##*/}"; do
+
+		while pgrep -xf "^bash ${0##*/}$"; do
 			sleep 1;
 			[ $((i++)) -gt 5 ] && break
 		done
-		pgrep -af "^$app\b" || $app # -a for debug
+		# Testing if actual app is still/already running
+		# -a to clarify issues when reading log file
+		# x is not necessary since pattern is enclosed in ^$, but to be sure.
+		# Some apps are meant to be running in the background, like mpd, so
+		#   no && { errormsg && exit; } here
+		pgrep -axf "^$app$" || $app
 		for ((i=0; i<${#files[@]}; i++)); do
-			[ ${lastmods[$i]} -lt "`stat -c %Y "${tmpfiles[$i]}"`" ] && modified=t
+			[ ${lastmods[$i]} -lt "`stat -c %Y "${tmpfiles[i]}"`" ] && modified=t
 		done
 		[ -v modified ] && {
-			cd ~/repos/general
 			for ((i=0; i<${#files[@]}; i++)); do
-				gpg --batch -se --output ${files[$i]} --yes -R *dete ${tmpfiles[$i]} \
-					|| zenity --error --text="GPG couldn’t encrypt “${tmpfiles[$i]}”."
-				git add ${files[$i]} || local git_error=t
+				unset dont_update
+				#   batch, sign & encrypt                   hidden-recipient
+				gpg --batch -se --output ${tmpfiles[i]}.gpg --yes -R *$ME_FOR_GPG ${tmpfiles[i]} 2>$elog \
+					|| i3-nagbar -m "GPG couldn’t encrypt ${tmpfiles[i]##*/}. See $elog for details."
+				local old_size=`stat -Lc %s "${files[i]}"` # dereference symlinks!
+				local new_size=`stat -c %s "${tmpfiles[i]}.gpg"`
+				[[ "$old_size" =~ ^[0-9]+$ ]] && [[ "$new_size" =~ ^[0-9]+$ ]] || {
+					i3-nagbar -m "Couldn’t compute file sizes for ${tmpfiles[i]##*/}.gpg ($new_size) and ${files[i]##*/} ($old_size)."
+					exit 6
+				}
+
+				local d=+$((new_size-old_size))
+
+				# Firefox tends to alter files a little for no reason :D
+				[ "$app_name" = firefox -o "$app_name" = firefox-bin ] \
+					&& [ "${d//[+-]/}" -lt 11 ] \
+					&& local dont_update=t
+				[ $new_size -lt $old_size ] && {
+					zenity --question --no-wrap --text "$app_name file $tmpfile has changed, but seem to have lower size.\n\nOLD_SIZE: $((old_size/1024)) KiB   NEW_SIZE: $((new_size/1024)) KiB, ${d/+-/-} B change.\n\nDo you want to replace it in the repository?" \
+					       --ok-label='Replace' --cancel-label='Keep old' || local dont_update=t
+				}
+				[ -v dont_update ] || {
+					cp ${tmpfiles[i]}.gpg ${files[i]} # not mv! mv erases symlinks.
+					[ "`file --mime-type ${files[i]} | sed -nr 's/.*\s(\S+)/\1/p'`" = inode/symlink ] || {
+						i3-nagbar -m "${files[i]} is not a symlink! Will exit nao."
+						exit 7
+					}
+					pushd ~/repos/general
+					git add ${files[i]#/} 2>>$elog && {
+						git commit -m "Updated ${files[i]##*/} for $app_name ${d/+-/-} B." 2>>$elog \
+							&& git push 2>>$elog \
+							|| i3-nagbar -m "Couldn’t push ${files[i]##*/} for $app_name. See $elog for details."
+						[ 1 -eq 1 ]
+					}|| i3-nagbar -m "Couldn’t add ${files[i]##*/} for $app_name. See $elog for details."
+					popd
+				}
 			done
-			[ -v git_error ] || {
-				git commit -m "Updated private data for $app." \
-					&& git push || local git_error=t
-			}
-			[ -v git_error ] && zenity --error \
-				--text="Couldn’t push changes for “$app” files.\n${tmpfiles[@]}"
 		}
 		for ((i=0; i<${#files[@]}; i++)); do
-			rm ${tmpfiles[$i]}
+			rm ${tmpfiles[i]} ${tmpfiles[i]}.gpg
 		done
 	}
 }
@@ -74,7 +102,7 @@ case ${0##*/} in
 		if pgrep -f $firefox &>/dev/null; then
 			# zenity --info --text="$*"
 			# $@ is not a plain link, but firefox understands it.
-			$firefox -new-tab $@
+			$firefox -new-tab "$@"
 		else
 			run_app $firefox \
 				~/.mozilla/firefox/profile.default/key3.db.gpg \
@@ -89,6 +117,9 @@ case ${0##*/} in
 		run_app /usr/bin/mpd
 		;;
 	mpdscribble)
+		# Actually mpdscribble would require a delay, because it daemonizes
+		#   after launch (if only --no-daemon option hasn’t been passed),
+		#   but since its config isn’t a subject to change, it’s not relevant.
 		run_app /usr/bin/mpdscribble \
 			~/.mpdscribble/mpdscribble.conf.gpg
 		;;
@@ -97,14 +128,17 @@ case ${0##*/} in
 			~/.purple/accounts.xml.gpg
 		;;
 	shutdown)
+		~/.i3/on_quit.sh
 		kill -HUP `pgrep gpg-agent` # clear cache
 		sudo /sbin/shutdown $@
 		;;
 	*)
-		cat <<EOF
+		cat <<"EOF"
 Usage:
     Just use symbolic links:
-    ~/bin/firefox -> ~/bin/run_app.sh
+    ln -s ~/bin/run_app.sh ~/bin/firefox
+
+Don’t forget to `export PATH="$HOME/bin:$PATH"`!
 EOF
 		exit 3
 		;;
