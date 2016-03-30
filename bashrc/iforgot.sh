4f2272b1280f24570c7891a36b5f4e6ca15bd892
deterenkelt <deterenkelt.github@gmail.com>
2016-03-31 01:37:04 +0300

    Upd

--- bashrc/iforgot.sh
+++ bashrc/iforgot.sh
@@ -336,6 +336,22 @@ cat <<"EOF"
 EOF
 }
 
+iforgot-ffmpeg-opts-for-encoding() {
+cat <<EOF
+ffmpeg_opts="-vcodec libx264 -b 1024k -s 800x480 \
+             -acodec libfaac -ab 128k \
+             -flags +loop+mv4 -cmp 256 \
+             -partitions +parti4x4+parti8x8+partp4x4+partp8x8+partb8x8 \
+             -me_method hex -me_range 16 \
+             -subq 7 -trellis 1 \
+             -refs 5 -bf 0 \
+             -flags2 +mixed_refs \
+             -coder 0 -g 250 -keyint_min 25 \
+             -sc_threshold 40 \
+             -i_qfactor 0.71 -qmin 10 -qmax 51 -qdiff 4"
+EOF
+}
+
 iforgot-tee-usage() {
 cat <<"EOF"
     wget -O - http://example.com/dvd.iso \
@@ -919,7 +935,7 @@ EOF
 
 iforgot-hierarchy-of-linux-fs() { echo -e '\tman hier'; }
 
-iforgot-ssh-passing-through-gateway() {
+iforgot-ssh-jump-through-gateway() {
 cat<<EOF
 1. Tunneling
 ssh -f gate_user@gate_ip -L [localhost:]<big_port>:<host_behind_gw>:<sshd_port_on_that_host> -N && ssh -p<big_port> <user_at_host_behind>@localhost
@@ -985,6 +1001,7 @@ iforgot-lsof-fuser() {
 cat <<EOF
     fuser -m <filesystem>
     lsof | grep <filesystem>
+    vmtouch <file>
 EOF
 }
 
