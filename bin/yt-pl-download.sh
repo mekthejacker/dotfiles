53d6951b614cf88921a746caefcee197ed5d98fc
deterenkelt <deterenkelt.github@gmail.com>
2017-10-26 17:50:55 +0300

    upd

--- bin/yt-pl-download.sh
+++ bin/yt-pl-download.sh
@@ -2,8 +2,14 @@
 
 # yt-pl-download.sh
 # Downloads a youtube playlist by passed URL and saves audio tracks as AAC.
-# to specified folder. Change --audio-format from ‘best’ to ‘mp3’,
-# if you need files in mp3.
+# to specified folder. Remembers what was already downloaded, so works fast.
+# Change --audio-format from ‘best’ to ‘mp3’, if you need files in mp3.
+
+
+ # Options for slow connections
+#
+# man -P"less -p '^\s*Format selection examples'" youtube-dl
+# man -P"less -p '^\s*FORMAT'" youtube-dl
 
 yt-pl() {
 	local dir="$1" playlist="$2" format='aac'  # best|aac|flac|mp3|m4a|opus|vorbis|wav
@@ -21,3 +27,4 @@ yt-pl() {
 }
 
 yt-pl "$1" "$2"
+
